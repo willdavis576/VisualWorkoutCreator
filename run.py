fcf0f49e02dd270b5b6e5e384f830c641b97f2fb
Will Davis <willdavis576@gmail.com>
2022-04-25 14:36:42 +0100

    Created a new main script with a csv file containing the required workout data, handling this with pandas.

--- run.py
+++ run.py
@@ -20,32 +20,60 @@ Saving is required, as well as tracking progress.
 from time import sleep
 
 
-class Sessions:
-    def __init__(self, sessionNum, HIITround1, GVT1, HIITround2, GVT2):
-        self.sessionNum = sessionNum
-        self.HIITround1 = HIITround1
-        self.GVT1 = GVT1
-        self.HIITround2 = HIITround2
-        self.GVT2 = GVT2
+# class Sessions:
+#     def __init__(self, sessionNum, HIITround1, GVT1, HIITround2, GVT2):
+#         self.sessionNum = sessionNum
+#         self.HIITround1 = HIITround1
+#         self.GVT1 = GVT1
+#         self.HIITround2 = HIITround2
+#         self.GVT2 = GVT2
         
-    def printShit(self):
-        print(self.sessionNum, self.HIITround1, self.GVT1, self.HIITround2, self.GVT2)
+#     def printShit(self):
+#         print(self.sessionNum, self.HIITround1, self.GVT1, self.HIITround2, self.GVT2)
         
 
-#i knowwwww eval on input is bad, shut up, it's temporary.
-sessionNum = 1 #int(input("insert your session number. "))
-HIITround1 = (6,35,45,2) #eval(input("insert: sets amount, set duration, set rest, final rest "))
-GVT1 = (1,10,10) #eval(input("insert: activity 1 or 2, sets amount, reps amount "))
-HIITround2 = (6,35,45,2) #eval(input("insert: sets amount, set duration, set rest, final rest "))
-GVT2 = (1,10,10) #eval(input("insert: activity 1 or 2, sets amount, reps amount "))
+# #i knowwwww eval on input is bad, shut up, it's temporary.
+# sessionNum = 1 #int(input("insert your session number. "))
+# HIITround1 = (6,35,45,2) #eval(input("insert: sets amount, set duration, set rest, final rest "))
+# GVT1 = (1,10,10) #eval(input("insert: activity 1 or 2, sets amount, reps amount "))
+# HIITround2 = (6,35,45,2) #eval(input("insert: sets amount, set duration, set rest, final rest "))
+# GVT2 = (1,10,10) #eval(input("insert: activity 1 or 2, sets amount, reps amount "))
 
-session = Sessions(sessionNum, HIITround1, GVT1, HIITround2, GVT2)
-session.printShit()
+# session = Sessions(sessionNum, HIITround1, GVT1, HIITround2, GVT2)
+# session.printShit()
 
 '''
 I need to write some sort of empty framework than can be populated and displayed on screen
 maybe QT?
 '''
+
+import pandas as pd
+
+bookDf = pd.read_csv("book.csv")
+
+def main2(bookDf, sessionNum):
+    weekContents = bookDf.loc[sessionNum-1,:]
+    HIIT = []
+    GVT = []
+    a = weekContents[2].split(",")
+    if "HIIT" in a: HIIT.append(a)
+    else: GVT.append(a)
+    b = weekContents[3].split(",")
+    if "HIIT" in b: HIIT.append(b)
+    else: GVT.append(b)
+    c = weekContents[4].split(",")
+    if "HIIT" in c: HIIT.append(c)
+    else: GVT.append(c)
+    d = weekContents[5].split(",")
+    if "HIIT" in d: HIIT.append(d)
+    else: GVT.append(d)
+    print("Welcome to Session {seshNum}. This week we will be covering {desc}. For GVT, you have the option for {GVT} for the first section and {GVT2} for second section.".format(seshNum = sessionNum, desc = weekContents[1], GVT= GVT[0], GVT2 = GVT[1]))
+    choices = input("Please enter in the form firstChoice,secondChoice: ")
+    print("You have chosen {one} and {two}.".format(one = GVT[0][int(choices[0])-1], two = GVT[1][int(choices[2])-1]))
+
+main2(bookDf, 1)
+
+
 
 def main(session):
     step = 0
@@ -86,4 +114,4 @@ def main(session):
     
     
     
-main(session)
+# main(session)
